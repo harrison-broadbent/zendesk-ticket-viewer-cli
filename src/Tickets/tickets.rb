1f3f3b9ef5055570b7151850ff9098fef462fbc9
Harrison Broadbent <harrisonbroadbent@gmail.com>
2021-05-15 14:39:16 +1000

    Added error handling to Tickets class, added extra check to Application to handle empty response
    Added error handling to the methods in the Tickets classs -
    	- catches NetworkError if the API is unavailable
    	- catches RecordNotFound if there are no records available / the record specified does not exist
    	- catches all other errors and prints a generic message, with the error message
    
    added a check to Application to handle an empty reponse from the Tickets class.
    This can happen if the API / network drops out partway through viewing all the pages of tickets.

--- src/Tickets/tickets.rb
+++ src/Tickets/tickets.rb
@@ -5,45 +5,69 @@ class Tickets
 
   def initialize(url, username, api_token)    
     @client = ZendeskAPI::Client.new do |config| 
-    
       config.url = url
       config.username = username
       config.token = api_token
     end
-
   end
 
   def get_all()
-    tickets = []
-    @client.tickets.all! do |ticket| 
-      tickets.append(Ticket.new(ticket))
+    begin
+      tickets = []
+      @client.tickets.all! do |ticket| 
+        tickets.append(Ticket.new(ticket))
+      end
+      tickets
+    rescue ZendeskAPI::Error::RecordNotFound
+      puts "An error has occured."
+      puts "Record not found."
+    rescue ZendeskAPI::Error::NetworkError
+      puts "An error has occured."
+      puts "API is unavailable. Check your network and https://status.zendesk.com ."
+    rescue => error
+      puts "An error has occured."
+      puts error.to_s
     end
-
-    tickets
   end
 
   def get_page(page_number, per_page)
-    tickets = []
-    @client.tickets.page(page_number).per_page(per_page).fetch!.each do |ticket|
-      tickets.append(Ticket.new(ticket))
+    begin
+      tickets = []
+      @client.tickets.page(page_number).per_page(per_page).fetch!.each do |ticket|
+        tickets.append(Ticket.new(ticket))
+      end
+      tickets
+    rescue ZendeskAPI::Error::RecordNotFound
+      puts "An error has occured."
+      puts "Record not found."
+    rescue ZendeskAPI::Error::NetworkError
+      puts "An error has occured."
+      puts "API is unavailable. Check your network and https://status.zendesk.com ."
+    rescue => error
+      puts "An error has occured."
+      puts error.to_s
     end
-
-    tickets
   end
 
   def get_single(ticket_id)
-
     begin
       ticketData = @client.tickets.find!(:id => ticket_id)
       return Ticket.new(ticketData)
     rescue ZendeskAPI::Error::RecordNotFound
-      puts
-      puts "An error has occured." 
-      puts "Could not find a ticket with that ID."
-      puts "Please try again with a valid ID."
-      puts "You may want to try viewing all tickets to see their associated IDs. "
-      puts
+      puts "An error has occured."
+      puts "Record not found."
+    rescue ZendeskAPI::Error::NetworkError
+      puts "An error has occured."
+      puts "API is unavailable. Check your network and https://status.zendesk.com ."
+    rescue => error
+      puts "An error has occured."
+      puts error.to_s
     end
+  end
+
+  def get_test_ticket()
+    ticketData = @client.tickets.first
+    return Ticket.new(ticketData)
   end
 
 end
